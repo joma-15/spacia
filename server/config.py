02c2298caeb846ffc222ae5c029cf35f20046c60
jhomaa <adelzarajhonmarcel@gmail.com>
2026-08-19 11:07:18 +0800

    uncomented the database ssl certificate

--- server/config.py
+++ server/config.py
@@ -28,11 +28,11 @@ class Config:
         "pool_timeout": 30,
         "pool_recycle": 1800,
         "pool_pre_ping": True,
-        # "connect_args": {
-        #     "ssl": {
-        #         "ca": str(BASE_DIR / "ca.pem")
-        #     }
-        # },
+        "connect_args": {
+            "ssl": {
+                "ca": str(BASE_DIR / "ca.pem")
+            }
+        }, 
     }
 
     UPLOAD_FOLDER = BASE_DIR / "uploads"
